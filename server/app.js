50c1ca4cf9720786e756f8614075eac75175d7f3
Dmytro Lysenko <demansn@gmail.com>
2024-11-26 17:26:22 +0200

    Fixed path for .env file

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ import {MongoDBAdapter} from "./db/MongoDBAdapter.js";
 
 dotenv.config(path.join(process.env.PWD, '.env'));
 
+console.log(process.env);
+
 // const bot  = new TelagramBot(process.env.BOT_TOKEN);
 
 const options = {
@@ -16,13 +18,12 @@ const options = {
     cert: fs.readFileSync('server.cert'),
 };
 const taskTemplate = JSON.parse(fs.readFileSync('./task_templates.json', "utf-8"))
-const isDev = process.env.NODE_ENV === 'development';
 const db = new MongoDBAdapter(process.env.MONGO_URL, process.env.MONGO_DB_NAME, taskTemplate);
 
 await db.connect();
 // const fileDB = new FileDatabaseAdapter('./', taskTemplate);
 
-const game = new GameServer(process.env.BOT_TOKEN, db, isDev);
+const game = new GameServer(process.env.BOT_TOKEN, db);
 
 const httpServer = new HTTPServer(options);
 
